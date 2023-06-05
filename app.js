a823290dcf61915c57243e8ea26f8a73cf517a04
Wilder <delforceh@gmail.com>
2023-06-05 16:19:58 +0200

    quest4bis

--- app.js
+++ app.js
@@ -13,19 +13,21 @@ const welcome = (req, res) => {
 
 app.get("/", welcome);
 
+
 const movieHandlers = require("./movieHandlers");
+const { validateMovie, validateUser } = require("./validates")
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie)
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.put("/api/movies/:id",validateMovie, movieHandlers.updateMovie)
 
 const useHandlers = require("./useHandlers");
 
 app.get("/api/users", useHandlers.getUsers);
 app.get("/api/users/:id", useHandlers.getUserById);
-app.post("/api/users", useHandlers.postUser);
-app.put("/api/users/:id", useHandlers.updateUser);
+app.post("/api/users",validateUser, useHandlers.postUser);
+app.put("/api/users/:id",validateUser, useHandlers.updateUser);
 
 app.listen(port, (err) => {
   if (err) {
