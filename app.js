e0c54c649f83747c0c1fdeb3ecbec11b754e0fed
Wilder <delforceh@gmail.com>
2023-06-12 16:42:34 +0200

    quest8

--- app.js
+++ app.js
@@ -17,14 +17,15 @@ app.get("/", welcome);
 const movieHandlers = require("./movieHandlers");
 const { validateMovie, validateUser } = require("./validates")
 
+const useHandlers = require("./useHandlers");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth");
+
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.post("/api/movies",verifyToken, validateMovie, movieHandlers.postMovie);
 app.put("/api/movies/:id",validateMovie, movieHandlers.updateMovie)
 app.delete("/api/movies/:id",movieHandlers.deleteMovie)
 
-const useHandlers = require("./useHandlers");
-const {hashPassword}  = require("./auth.js");
 
 
 app.get("/api/users", useHandlers.getUsers);
@@ -32,6 +33,14 @@ app.get("/api/users/:id", useHandlers.getUserById);
 app.post("/api/users",hashPassword, validateUser, useHandlers.postUser);
 app.put("/api/users/:id",hashPassword, validateUser, useHandlers.updateUser);
 app.delete("/api/users/:id", useHandlers.deleteUser)
+
+
+app.post(
+  "/api/login",
+  useHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+
 
 app.listen(port, (err) => {
   if (err) {
